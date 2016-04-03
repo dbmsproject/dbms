5bdb7baaa66c5accc3c0d9601cda1762fc5c49e0
avalanche557 <abhinavkarn55@gmail.com>
2016-04-03 17:05:41 +0530

    change in login.php

--- login1.php
+++ login1.php
@@ -7,12 +7,9 @@
 			# code...
 			$login = "SELECT * FROM doctor WHERE email = '$email' && password='$password'";
 			$query = mysql_query($login);
-			if (mysql_num_rows($query)==1) {
-				# code...
-				echo "welcome to your profile ";
+			if (mysql_num_rows($query)==1) {	
 			}
 			else {
-				echo "can't be login";
 			}
 		}
 
@@ -23,10 +20,29 @@
 		<meta charset="utf-8">
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="css/stylesheet1.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		
 		<title>hospital management</title>
 	</head>
 	<body>
-		<button>logout</button>
+		<div  id ="header"> 
+			<h1>MNNIT HOSPITAL</h1>
+		</div>
+		<div id="header2">
+			<div id="navbar">
+				<nav>
+					<ul>
+						<li><a href ="index.php">Home</a></li>
+						<li><a href ="reg.php">Profile</a></li>
+						<li><a href ="index1.php">Logout</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div id ="content">
+			
+		</div>
+		<div id="footer"></div>
+	</body>
+</html>
